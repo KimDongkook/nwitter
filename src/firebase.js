8140686efde62b68cd7ebccd03bed7e3e7671db4
KimDongKook <pottes11@naver.com>
2021-10-24 17:14:59 +0900

    hide firebase app key

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA2TSrm0hWgno83q6EVaqgRN8RKXWOB4GU",
-  authDomain: "nwitter-d0e9d.firebaseapp.com",
-  projectId: "nwitter-d0e9d",
-  storageBucket: "nwitter-d0e9d.appspot.com",
-  messagingSenderId: "80723979391",
-  appId: "1:80723979391:web:7b731b9f1e111a4cf4e34a"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export default initializeApp(firebaseConfig);
